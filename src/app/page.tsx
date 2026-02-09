cec5b40063797841a103b486bbd3b701dbdec570
mingile <vhrehf1795@gmail.com>
2026-02-09 23:56:04 +0900

    lint check & build test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { PlusButton } from "./PlusButton";
-import { Row } from "./Row";
 import { WorkoutSessionClient } from "./WorkoutSessionClient";
 
 export default function Home() {
-
   return (
     <>
       <header className="sticky top-0 z-50 bg-background/95 backdrop-blur border-b supports-[backdrop-filter]:bg-background/60">
